44162a4b7c495a1ad83bd6360384e4b9ad726baf
jihadul-islam <jihadulislam708@gmail.com>
2020-09-08 15:29:28 -0800

    last commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AllUser from "../src/Component/AllUser/AllUser";
+import './App.css';
+import Header from './Component/Header/Header';
+
+
+function App() {
+  
+  
+
+  return (
+  <div>
+  <Header></Header>
+   <AllUser></AllUser>
+  </div>
+   
+    
+    
+  );
+  
+}
+
+export default App;
